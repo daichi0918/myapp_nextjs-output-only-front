8434c2995c38ac2fd62685b55c07b34875173582
daichi0918 <kayasugadaichi@icloud.com>
2024-11-17 17:23:27 +0900

    todoのhooks処理実装完了

--- src/components/templates/Todo/index.tsx
+++ src/components/templates/Todo/index.tsx
@@ -6,14 +6,13 @@
  */
 import styles from './styles.module.css';
 import Link from 'next/link';
-import { EventType } from '@/interfaces/Event';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFile, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { TodoContext } from '@/contexts/TodoContext';
-import { TodoType } from '@/interfaces/Todo';
+import { useTodo } from '@/hooks/Todo';
 import { useRouter } from 'next/router';
 import InputForm from '@/components/atoms/InputForm';
-import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+
 import TodoList from '@/components/organisms/TodoList';
 
 /**
@@ -21,47 +20,12 @@ import TodoList from '@/components/organisms/TodoList';
  * @returns {JSX.Element}
  */
 export const TodoTemplate = () => {
-  /* state定義 */
-  const { originalTodoList, setOriginalTodoList } = useContext(TodoContext);
-  const [searchKeyWord, setSearchKeyWord] = useState<string>('');
-  // const router = useRouter();
-
-  /* action定義 */
-  /**
-   * 検索キーワード更新処理
-   * @param {*} e
-   */
-  const handleChangeSearchKeyword: EventType['onChangeInput'] = useCallback(
-    (e) => setSearchKeyWord(e.target.value),
-    []
-  )
-
-  /**
-   * 表示用TodoList
-   */
-    const showTodoList = useMemo(() => {
-      return originalTodoList.filter((todo) => {
-        const regexp = new RegExp('^' + searchKeyWord, 'i');
-        return todo.title.match(regexp);
-      });
-    }, [searchKeyWord, originalTodoList]);
-
-    
-  /**
-   * todoの削除処理
-   * @param { number } targetId
-   * @param { string } taskName
-   */
-  const handleDeleteTodoTask = (targetId: number, taskName: string) => {
-    if (window.confirm(`「${taskName}」を削除していいですか？`)) {
-      const newTodoList = originalTodoList.filter(
-        (todo) => todo.id !== targetId
-      );
-      setOriginalTodoList(newTodoList);
-    }
-  };
-
-
+  const {
+    showTodoList,
+    searchKeyWord,
+    handleDeleteTodoTask,
+    handleChangeSearchKeyword
+  } = useTodo();
   return (
     <>
       <h1 className={styles.title}>TodoList</h1>
@@ -75,28 +39,6 @@ export const TodoTemplate = () => {
           />
         </div>
         <div className={styles.area}>
-          {/* <ul className={styles.todolist}>
-            {
-              showTodoList.length > 0 && (
-                showTodoList.map((todo: TodoType) => (
-                  <li className={styles.todoitem} key={todo.id}>
-                  <span className={styles.task}>{todo.title}</span>
-                  <div className={styles.todo_top_icons}>
-                    <Link href={`/todo/detail/${todo.id}`} className={styles.icon_wrapper}>
-                        <FontAwesomeIcon icon={faFile} size='lg'/>
-                      </Link>
-                      <Link href={`/todo/edit/${todo.id}`} className={styles.icon_wrapper}>
-                        <FontAwesomeIcon icon={faPenToSquare} size='lg'/>
-                      </Link>
-                      <div className={styles.icon_wrapper}>
-                        <FontAwesomeIcon icon={faTrashCan} size='lg' onClick={() => handleDeleteTodoTask(todo.id, todo.title)}/>
-                      </div>
-                  </div>
-                </li>
-                ))
-              )
-            }
-          </ul> */}
           <TodoList showTodoList={showTodoList} handleDeleteTodoTask={handleDeleteTodoTask}/>
         </div>
       </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-  console.log('ミドルウェアsss');
   return NextResponse.next();
 };
 
